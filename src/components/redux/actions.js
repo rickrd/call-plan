5e429675b4c83148ba5adbbd5fe66214e53169d5
rickrd <desouzaricardoc@gmail.com>
2019-12-31 05:51:49 -0300

    created initial redux actions & reducers

---
+++ src/components/redux/actions.js
@@ -0,0 +1,5 @@
+export const ADD_CALL = 'ADD_CALL'
+
+export const addCall = call => {
+  return { type: ADD_CALL, call }
+}
---
+++ src/components/redux/reducers.js
@@ -0,0 +1,20 @@
+import ADD_CALL from './actions'
+
+const initialState = {
+  calls: []
+}
+
+const calls = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CALL:
+      return Object.assign({}, state, action.call)
+    default:
+      return state
+  }
+}
+
+export default function reducers(state = initialState, action) {
+  return {
+    calls: calls(state.calls, action)
+  }
+}
