84dc3894664db537e9517851a5d96e5e6574f6da
rickrd <desouzaricardoc@gmail.com>
2020-01-05 20:45:21 -0300

    installed material-ui, added table to list calls, removed call list item

--- src/components/organisms/CallList.js
+++ src/components/organisms/CallList.js
@@ -2,26 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 
-import CallListItem from '../molecules/CallListItem'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
 const CallListWrapper = styled.div`
   margin-top: 20px;
 
   h2 {
-    margin: 0 auto;
     text-align: center;
     color: #fff;
-  }
-`
-
-const CallListItemWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  @media (min-width: 1280px) {
-    align-items: flex-start;
+    font-size: 16px;
+    font-weight: 300;
   }
 `
 
@@ -30,11 +26,32 @@ const CallList = props => {
   return calls && calls.length > 0 ? (
     <CallListWrapper>
       <h2>Lista de chamadas calculadas:</h2>
-      <CallListItemWrapper>
-      {calls.map((call, index) => (
-        <CallListItem call={call} key={index}></CallListItem>
-      ))}
-      </CallListItemWrapper>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Origem</TableCell>
+              <TableCell align="left">Destino</TableCell>
+              <TableCell align="left">Tempo (min.)</TableCell>
+              <TableCell align="left">Plano</TableCell>
+              <TableCell align="left">Com FaleMais</TableCell>
+              <TableCell align="left">Sem FaleMais</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {calls.map((call, index) => (
+              <TableRow key={index}>
+                <TableCell align="left">{call.from}</TableCell>
+                <TableCell align="left">{call.to}</TableCell>
+                <TableCell align="left">{call.time}</TableCell>
+                <TableCell align="left">{call.plan}</TableCell>
+                <TableCell align="left">R$ {call.withPlan.toString().replace('.', ',')}</TableCell>
+                <TableCell align="left">R$ {call.withoutPlan.toString().replace('.', ',')}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </CallListWrapper>
   ) : null
 }
